ceda66189db6ca8262d473fdf1e2776311265aeb
Adithyan T T <adithyantt00@gmail.com>
2026-01-02 23:49:03 +0530

    updating the popular categories

---
+++ lib/screens/jobs/find_job_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class FindJobsScreen extends StatelessWidget {
+  const FindJobsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text("Jobsify"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              // later: post job bottom sheet
+            },
+          ),
+        ],
+      ),
+      body: const Center(child: Text("Jobs list will come here")),
+    );
+  }
+}
